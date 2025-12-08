e4b4a66730890f2377c809fe60355259e4811ba1
Harold Favereau <harold.favereau@liksi.fr>
2025-12-08 21:15:18 +0100

    day 8 part 2

--- aoc-2025-8/src/main.rs
+++ aoc-2025-8/src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use anyhow::anyhow;
-use std::collections::HashSet;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
@@ -9,162 +8,145 @@ use std::{
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point(u128, u128, u128);
+impl Point {
+    fn norm(&self) -> u128 {
+        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+    }
 
-fn norm(point: &Point) -> u128 {
-    point.0 * point.0 + point.1 * point.1 + point.2 * point.2
-}
-
-fn distance(point1: &Point, point2: &Point) -> u128 {
-    norm(&Point(
-        point1.0.abs_diff(point2.0),
-        point1.1.abs_diff(point2.1),
-        point1.2.abs_diff(point2.2),
-    ))
+    fn distance(&self, point: &Point) -> u128 {
+        Point(
+            self.0.abs_diff(point.0),
+            self.1.abs_diff(point.1),
+            self.2.abs_diff(point.2),
+        )
+        .norm()
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct Pair {
-    first: Point,
-    second: Point,
+    first: usize,
+    second: usize,
     dist: u128,
 }
 
-fn pair_from_points(point1: &Point, point2: &Point) -> Pair {
-    Pair {
-        first: *point1,
-        second: *point2,
-        dist: distance(point1, point2),
+impl Pair {
+    fn new(point1: &Point, point2: &Point, index1: usize, index2: usize) -> Pair {
+        Pair {
+            first: index1,
+            second: index2,
+            dist: point1.distance(point2),
+        }
     }
 }
 
-fn insert_sorted(pairs: &mut Vec<Pair>, pair: Pair) {
-    match pairs.binary_search_by(|t_pair| t_pair.dist.cmp(&pair.dist)) {
-        Ok(pos) => pairs.insert(pos, pair),
-        Err(pos) => pairs.insert(pos, pair),
+struct Kruskal {
+    parents: Vec<usize>,
+    sizes: Vec<usize>,
+}
+
+impl Kruskal {
+    fn with_capacity(size: usize) -> Self {
+        Self {
+            parents: (0..size).collect(),
+            sizes: vec![1; size],
+        }
+    }
+
+    fn find(&mut self, i: usize) -> usize {
+        let mut j = i;
+        while self.parents[j] != j {
+            self.parents[j] = self.parents[self.parents[j]];
+            j = self.parents[j];
+        }
+        j
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let x = self.find(x);
+        let y = self.find(y);
+
+        if x == y {
+            return;
+        }
+
+        if self.sizes[x] >= self.sizes[y] {
+            self.parents[y] = x;
+            self.sizes[x] += self.sizes[y];
+        } else {
+            self.parents[x] = y;
+            self.sizes[y] += self.sizes[x];
+        }
+    }
+
+    fn size_forests(&self) -> Vec<usize> {
+        self.parents
+            .iter()
+            .enumerate()
+            .filter(|(parent, x)| **x == *parent)
+            .filter_map(|(_, x)| Some(self.sizes[*x]).take_if(|v| *v != 1))
+            .collect()
     }
 }
 
 struct Network {
     points: Vec<Point>,
+    edges: Vec<Pair>,
     count_pairs: usize,
 }
 
 impl Network {
-    fn compute_smallest_distances(&self) -> Vec<Pair> {
-        let mut current_distances = Vec::with_capacity(self.points.len() * self.points.len() / 2);
-        let mut curr_points = Vec::with_capacity(self.points.len());
-        let mut curr_max = 0;
-        for point in self.points.iter() {
-            for already_point in curr_points.iter() {
-                let pair = pair_from_points(point, already_point);
-                if current_distances.len() < self.count_pairs {
-                    curr_max = curr_max.max(pair.dist);
-                    insert_sorted(&mut current_distances, pair);
-                } else if curr_max > pair.dist {
-                    current_distances.pop();
-                    insert_sorted(&mut current_distances, pair);
-                    curr_max = current_distances.last().unwrap().dist;
-                }
-            }
+    fn new(points: Vec<Point>, count_pairs: usize) -> Self {
+        let mut edges: Vec<Pair> = (0..points.len())
+            .flat_map(|i| {
+                (i + 1..points.len())
+                    .map(|j| Pair::new(&points[i], &points[j], i, j))
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+        edges.sort_by_key(|pair| pair.dist);
 
-            curr_points.push(*point);
+        Network {
+            points,
+            edges,
+            count_pairs,
         }
-
-        current_distances
     }
 
-    fn find_smallest_distances_circuit_prod(&self) -> usize {
-        let pairs = self.compute_smallest_distances();
-        let mut circuits: Vec<HashSet<Point>> = vec![];
-        //ugly union find
-        for pair in pairs {
-            let point1 = pair.first;
-            let point2 = pair.second;
-            let index1 = circuits
-                .iter()
-                .enumerate()
-                .filter(|(_, circuit)| circuit.contains(&point1))
-                .map(|(idx, _)| idx)
-                .next();
-            let index2 = circuits
-                .iter()
-                .enumerate()
-                .filter(|(_, circuit)| circuit.contains(&point2))
-                .map(|(idx, _)| idx)
-                .next();
-            match (index1, index2) {
-                (None, None) => {
-                    circuits.push(HashSet::from([point1, point2]));
-                }
-                (None, Some(index2)) => {
-                    circuits[index2].insert(point1);
-                }
-                (Some(index1), None) => {
-                    circuits[index1].insert(point2);
-                }
-                (Some(index1), Some(index2)) => {
-                    if index1 != index2 {
-                        let points_to_add = circuits[index2].clone();
-                        circuits[index1].extend(points_to_add);
-                        circuits.remove(index2);
-                    }
-                }
+    fn kruskal_limited_part1(&self) -> usize {
+        let mut kruskal = Kruskal::with_capacity(self.points.len());
+
+        for edge in self.edges[0..self.count_pairs].iter() {
+            let x = kruskal.find(edge.first);
+            let y = kruskal.find(edge.second);
+
+            if x != y {
+                kruskal.union(x, y);
             }
         }
-        let mut circuits_length = circuits
-            .iter()
-            .map(|circuit| circuit.len())
-            .collect::<Vec<_>>();
-        circuits_length.sort();
-        circuits_length.reverse();
-        circuits_length.iter().take(3).product()
+        let mut size_forests = kruskal.size_forests();
+        size_forests.sort();
+        size_forests.iter().rev().take(3).product()
     }
 
-    fn find_last_pair_prod(&self) -> usize {
-        let pairs = self.compute_smallest_distances();
-        let mut circuits: Vec<HashSet<Point>> = vec![];
-        //ugly union find
-        for pair in pairs {
-            let point1 = pair.first;
-            let point2 = pair.second;
-            let index1 = circuits
-                .iter()
-                .enumerate()
-                .filter(|(_, circuit)| circuit.contains(&point1))
-                .map(|(idx, _)| idx)
-                .next();
-            let index2 = circuits
-                .iter()
-                .enumerate()
-                .filter(|(_, circuit)| circuit.contains(&point2))
-                .map(|(idx, _)| idx)
-                .next();
-            match (index1, index2) {
-                (None, None) => {
-                    circuits.push(HashSet::from([point1, point2]));
-                }
-                (None, Some(index2)) => {
-                    circuits[index2].insert(point1);
-                }
-                (Some(index1), None) => {
-                    circuits[index1].insert(point2);
-                }
-                (Some(index1), Some(index2)) => {
-                    if index1 != index2 {
-                        let points_to_add = circuits[index2].clone();
-                        circuits[index1].extend(points_to_add);
-                        circuits.remove(index2);
-                    }
-                }
+    fn kruskal_full_part2(&self) -> Result<u128> {
+        let mut kruskal = Kruskal::with_capacity(self.points.len());
+        let mut edge_count = 0;
+
+        for edge in self.edges.iter() {
+            let x = kruskal.find(edge.first);
+            let y = kruskal.find(edge.second);
+
+            if x != y {
+                kruskal.union(x, y);
+                edge_count += 1;
+            }
+
+            if edge_count == self.points.len() - 1 {
+                return Ok(self.points[edge.first].0 * self.points[edge.second].0);
             }
         }
-        let mut circuits_length = circuits
-            .iter()
-            .map(|circuit| circuit.len())
-            .collect::<Vec<_>>();
-        circuits_length.sort();
-        circuits_length.reverse();
-        circuits_length.iter().take(3).product()
+        Err(anyhow!("graph does not have a spanning tree"))
     }
 }
 
@@ -191,11 +173,13 @@ fn run(path: &str) -> Result<(String, String)> {
 }
 
 fn part1(network: &Network) -> usize {
-    network.find_smallest_distances_circuit_prod()
+    network.kruskal_limited_part1()
 }
 
-fn part2(network: &Network) -> usize {
-    network.find_last_pair_prod()
+fn part2(network: &Network) -> u128 {
+    network
+        .kruskal_full_part2()
+        .expect("graph does not have a spanning tree")
 }
 
 fn parse_file(path: &str) -> Result<Network> {
@@ -218,10 +202,7 @@ fn parse_file(path: &str) -> Result<Network> {
                 })
         })
         .collect::<Result<Vec<_>>>()?;
-    Ok(Network {
-        points,
-        count_pairs,
-    })
+    Ok(Network::new(points, count_pairs))
 }
 
 #[cfg(test)]
